905c512c329d86dcbd7c671436f8ba94ead7c823
GiovanniBurbi <giovanniburbi10@gmail.com>
2023-03-10 22:07:52 +0100

    refactoring

--- Tile.js
+++ Tile.js
@@ -1,31 +1,14 @@
 import * as THREE from 'three';
-import { math } from './math';
-import perlin from 'https://cdn.jsdelivr.net/gh/mikechambers/es6-perlin-module/perlin.js'
+import { Noise } from './noise';
 
 export class Tile {
   constructor(center, dim, gui, params) {
-    this.gui = gui
-    this.params = params
 
-    this.params.noise = {
-      octaves: 4,
-      persistance: 0.5,
-      lacunarity: 2,
-      scale: 8,
-      offsetX: 0,
-      offsetY: 0
-    }
+    this.initGUI(gui, params)
 
-    
+    this.noise = new Noise(params)
 
-    this.randOff = Math.random() * 20000 - 10000
-
-    const onNoiseChange = () =>  {
-      this.visualizeMap()
-      this.buildTerrain()
-    }
-
-    this.createNoiseRollup(onNoiseChange)
+    this.hScale = 10
 
     this.center = center
     this.dim = dim
@@ -39,6 +22,8 @@ export class Tile {
     this.mesh = new THREE.Mesh( this.geometry, this.material )
     this.mesh.rotateX( - Math.PI / 2)
 
+    this.num_vertex = this.mesh.geometry.attributes.position.count
+
     this.mesh.castShadow = false;
     this.mesh.receiveShadow = true;
 
@@ -46,98 +31,31 @@ export class Tile {
 
     this.width = this.mesh.geometry.attributes.position.count / (this.segments + 1 )
 
+    this.heightMap = this.noise.generateNoiseMap(this.width, this.num_vertex)
     this.buildTerrain()
     this.visualizeMap()
   }
 
-  generateNoiseMap() {
-    const heights = new Array(this.mesh.geometry.attributes.position.count)
+  initGUI(gui, params) {
+    this.gui = gui
+    this.params = params
 
-    let maxNoiseHeight = Number.NEGATIVE_INFINITY 
-    let minNoiseHeight = Number.POSITIVE_INFINITY
-
-    for (let y = 0; y < this.width; y++){
-      for(let x = 0; x < this.width; x++){
-        let noiseValue = this.computeHeight(x, y)
-
-        heights[y * this.width + x] = noiseValue 
-
-        if (noiseValue > maxNoiseHeight) {
-          maxNoiseHeight = noiseValue
-        }
-        if (noiseValue < minNoiseHeight) {
-          minNoiseHeight = noiseValue
-        }
-      }
+    this.params.noise = {
+      octaves: 4,
+      persistance: 0.5,
+      lacunarity: 2,
+      scale: 8,
+      offsetX: 0,
+      offsetY: 0
     }
 
-    for (let y = 0; y < this.width; y++){
-      for(let x = 0; x < this.width; x++){
-        heights[y * this.width + x] = math.invLerp(heights[y * this.width + x], minNoiseHeight, maxNoiseHeight)
-      }
-    }
-    return heights
-  }
-
-  computeHeight(x, y) {
-
-    let amplitude = 1
-    let frequency = 1
-    let noiseHeight = 0
-
-    let perlinNoise
-
-    const halfW = this.width / 2
-    const halfH = this.width / 2
-
-    for(let i = 0; i < this.params.noise.octaves; i++){
-      let xOff = (x - halfW) / this.params.noise.scale * frequency + this.params.noise.offsetX + this.randOff
-      let yOff = (y - halfH) / this.params.noise.scale * frequency + this.params.noise.offsetY + this.randOff
-
-      // this noise return values between -1 and 1
-      perlinNoise = perlin(xOff, yOff) * 2 - 1
-      noiseHeight += perlinNoise * amplitude
-
-      amplitude *= this.params.noise.persistance
-      frequency *= this.params.noise.lacunarity
+    const onNoiseChange = () =>  {
+      this.heightMap = this.noise.generateNoiseMap(this.width, this.num_vertex)
+      this.visualizeMap()
+      this.buildTerrain()
     }
 
-    return noiseHeight
-  }
-
-  buildTerrain() {
-    const rand = 10
-    let vertices = this.mesh.geometry.attributes.position.array.slice()
-    
-    const heights = this.generateNoiseMap()
-
-    for(let i = 0; i < heights.length; i++){
-      // vertices[i * 3 + 2] = heights[i]
-      vertices[i * 3 + 2] = heights[i] * (rand + rand) - rand
-    }
-
-    this.mesh.geometry.setAttribute('position', new THREE.BufferAttribute( new Float32Array(vertices), 3 ))
-
-    this.mesh.geometry.elementsNeedUpdate = true;
-    this.mesh.geometry.verticesNeedUpdate = true;
-    this.mesh.geometry.computeVertexNormals();
-  }
-
-  visualizeMap() {
-    const heights = this.generateNoiseMap()
-
-    let colors = []
-
-    for (let y = 0; y < this.width; y++){
-      for(let x = 0; x < this.width; x++){
-        let c = heights[y * this.width + x]
-        colors.push(c,c,c)
-      }
-    }
-
-    this.mesh.geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3))
-
-    this.mesh.geometry.materialNeedUpdate = true
+    this.createNoiseRollup(onNoiseChange)
   }
 
   createNoiseRollup(funcChange) {
@@ -148,5 +66,34 @@ export class Tile {
     rollup.add(this.params.noise, 'scale', 0.3, 30).onChange(funcChange)
     rollup.add(this.params.noise, 'offsetX', 0.0, 20, 0.1).onChange(funcChange)
     rollup.add(this.params.noise, 'offsetY', 0.0, 20, 0.1).onChange(funcChange)
+  }
+
+  buildTerrain() {
+    let vertices = this.mesh.geometry.attributes.position.array.slice()
+
+    for(let i = 0; i < this.heightMap.length; i++){
+      vertices[i * 3 + 2] = this.heightMap[i] * (2 * this.hScale) - this.hScale
+    }
+
+    this.mesh.geometry.setAttribute('position', new THREE.BufferAttribute( new Float32Array(vertices), 3 ))
+
+    this.mesh.geometry.elementsNeedUpdate = true;
+    this.mesh.geometry.verticesNeedUpdate = true;
+    this.mesh.geometry.computeVertexNormals();
+  }
+
+  visualizeMap() {
+    let colors = []
+
+    for (let y = 0; y < this.width; y++){
+      for(let x = 0; x < this.width; x++){
+        let c = this.heightMap[y * this.width + x]
+        colors.push(c,c,c)
+      }
+    }
+
+    this.mesh.geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3))
+
+    this.mesh.geometry.materialNeedUpdate = true
   }
 }
--- noise.js
+++ noise.js
@@ -1,104 +1,62 @@
-// https://github.com/simondevyoutube/ProceduralTerrain_Part3/tree/master/src
+import { math } from './math';
 import perlin from 'https://cdn.jsdelivr.net/gh/mikechambers/es6-perlin-module/perlin.js'
-import { createNoise2D } from 'simplex-noise';
 
-import {math} from './math.js';
-
-export const noise = (function() {
-
-  class PerlinWrapper {
-    constructor() {
-    }
-
-    noise2D(x, y) {
-      return perlin(x, y) * 2.0 - 1.0;
-    }
+export class Noise {
+  constructor(params) {
+    this.params = params
+    this.randOff = Math.random() * 20000 - 10000
   }
 
-  class SimplexWrapper {
-    constructor(seed) {
-      this.simplex = createNoise2D(seed)
+  computeHeight(x, y, width) {
+    let amplitude = 1
+    let frequency = 1
+    let noiseHeight = 0
+
+    let noiseValue
+
+    const halfW = width / 2
+    const halfH = width / 2
+
+    for(let i = 0; i < this.params.noise.octaves; i++){
+      let xOff = (x - halfW) / this.params.noise.scale * frequency + this.params.noise.offsetX + this.randOff
+      let yOff = (y - halfH) / this.params.noise.scale * frequency + this.params.noise.offsetY + this.randOff
+
+      noiseValue = perlin(xOff, yOff) * 2 - 1
+      noiseHeight += noiseValue * amplitude
+
+      amplitude *= this.params.noise.persistance
+      frequency *= this.params.noise.lacunarity
     }
 
-    noise2D(x, y) {
-      return this.simplex(x, y);
-    }
+    return noiseHeight
   }
 
-  class RandomWrapper {
-    constructor() {
-      this.values = {};
-    }
+  generateNoiseMap(width, dim) {
+    const heights = new Array(dim)
 
-    rand(x, y) {
-      const k = x + '.' + y;
-      if (!(k in this.values)) {
-        this.values[k] = Math.random() * 2 - 1;
+    let maxNoiseHeight = Number.NEGATIVE_INFINITY 
+    let minNoiseHeight = Number.POSITIVE_INFINITY
+
+    for (let y = 0; y < width; y++){
+      for(let x = 0; x < width; x++){
+        let noiseValue = this.computeHeight(x, y, width)
+
+        heights[y * width + x] = noiseValue 
+
+        if (noiseValue > maxNoiseHeight) {
+          maxNoiseHeight = noiseValue
+        }
+        if (noiseValue < minNoiseHeight) {
+          minNoiseHeight = noiseValue
+        }
       }
-      return this.values[k];
     }
 
-    noise2D(x, y) {
-      // Bilinear filter
-      const x1 = Math.floor(x);
-      const y1 = Math.floor(y);
-      const x2 = x1 + 1;
-      const y2 = y1 + 1;
-    
-      const xp = x - x1;
-      const yp = y - y1;
-    
-      const p11 = this.rand(x1, y1);
-      const p21 = this.rand(x2, y1);
-      const p12 = this.rand(x1, y2);
-      const p22 = this.rand(x2, y2);
-    
-      const px1 = math.lerp(xp, p11, p21);
-      const px2 = math.lerp(xp, p12, p22);
-    
-      return math.lerp(yp, px1, px2);
-    }
-  }
-
-  //FBM
-  class NoiseGenerator {
-    constructor(params) {
-      this.params = params;
-      this.init();
-    }
-
-    init() {
-      this.noise = {
-        simplex: new SimplexWrapper(this.params.seed),
-        perlin: new PerlinWrapper(),
-        rand: new RandomWrapper(),
-      };
-    }
-
-    Get(x, y) {
-      const xs = x / this.params.scale;
-      const ys = y / this.params.scale;
-      const noiseFunc = this.noise[this.params.noiseType];
-      const G = 2.0 ** (-this.params.persistence);
-      let amplitude = 1.0;
-      let frequency = 1.0;
-      let normalization = 0;
-      let total = 0;
-      for (let o = 0; o < this.params.octaves; o++) {
-        const noiseValue = noiseFunc.noise2D(
-            xs * frequency, ys * frequency) * 0.5 + 0.5;
-        total += noiseValue * amplitude;
-        normalization += amplitude;
-        amplitude *= G;
-        frequency *= this.params.lacunarity;
+    for (let y = 0; y < width; y++){
+      for(let x = 0; x < width; x++){
+        heights[y * width + x] = math.invLerp(heights[y * width + x], minNoiseHeight, maxNoiseHeight)
       }
-      total /= normalization;
-      return Math.pow(
-          total, this.params.exponentiation) * this.params.height;
     }
+    return heights
   }
-
-  return {
-    Noise: NoiseGenerator
-  }
-})();
+}
